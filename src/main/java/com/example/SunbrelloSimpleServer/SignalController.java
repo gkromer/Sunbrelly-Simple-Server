3d510c6c646e888b852350b38171648e33390fa7
Georg Kromer <georgkromer@protonmail.com>
2020-05-17 21:28:42 +0200

    made the Server receive and return a signal

--- src/main/java/com/example/SunbrelloSimpleServer/SignalController.java
+++ src/main/java/com/example/SunbrelloSimpleServer/SignalController.java
@@ -1,16 +1,35 @@
 package com.example.SunbrelloSimpleServer;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 @CrossOrigin
 public class SignalController {
+ Integer signal = 0;
+    Logger logger = Logger.getLogger(SignalController.class.getName());
     /**
      * @return: say hello
      */
     @PostMapping("/postSignal")
-    public String sayHello(@RequestBody String signal) {
+    public String receiveSignal(@RequestBody String signal) {
         System.out.println("Hello"+ signal);
         return "HELLOOOOOOOOO";
     }
+
+    /**
+     * @return: get's and returns a signal
+     */
+    @RequestMapping(value = "/getSignal", method = RequestMethod.GET)
+    public int sayHello(@RequestBody SignalDto signal) {
+        logger.log(Level.INFO, "Signal received = "+signal.getSignal());
+        return signal.getSignal();
+    }
+
+
 }
---
+++ src/main/java/com/example/SunbrelloSimpleServer/SignalDto.java
@@ -0,0 +1,17 @@
+package com.example.SunbrelloSimpleServer;
+
+
+public class SignalDto {
+    private int signal;
+
+    public SignalDto() {
+    }
+
+    public int getSignal() {
+        return signal;
+    }
+
+    public void setSignal(int signal) {
+        this.signal = signal;
+    }
+}
